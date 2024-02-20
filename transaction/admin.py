5f22ad9d9684db175e59b23ab88ee49bcdd7482d
MrYuriy <yuriybabiyk@gmail.com>
2024-02-20 11:03:16 +0100

    Inicialize  Transaction model

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "format_transaction_datetime",
+        "name",
+        "delivery"
+    )
+    search_fields = ["delivery"]
+
+    def format_transaction_datetime(self, obj):
+        return obj.transaction_datetime.strftime('%d-%m-%Y, %H:%M')
+
+admin.site.register(Transaction, TransactionAdmin)
--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+from delivery.models import Delivery
+from user.models import User
 
-# Create your models here.
+class Transaction(models.Model):
+    TRANSACTION_TYPE = (
+        ("Recive", "Recive"),
+        ("Relocate", "Relocate"),
+        ("Shiped", "Shiped"),
+        ("Utilization", "Utilization")
+    )
+    name = models.CharField(choices=TRANSACTION_TYPE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="transactions"
+    )
+    delivery = models.ForeignKey(
+        Delivery, on_delete=models.CASCADE, related_name="transactions"
+    )
+    transaction_datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.transaction_datetime.strftime('%d-%m-%Y, %H:%M')} - {self.name} {self.delivery.identifier}"
+    
