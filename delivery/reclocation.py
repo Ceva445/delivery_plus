25468717e87101c1d1e1d44cac1bb2cb5aa2b5c1
MrYuriy <yuriybabiyk@gmail.com>
2024-03-29 14:59:33 +0100

    Fix relocation error

--- delivery/reclocation.py
+++ delivery/reclocation.py
@@ -3,23 +3,28 @@ from datetime import datetime
 
 
 def relocate_delivery(user, delivery, to_location):
-    transaction_type = "Relocate"
+    transact_names = {
+            "Utulizacja": "Utulizacja",
+            "Shiped": "Shiped",
+            "ANULACJA": "Anulacja"
+            }
     delivery.transaction += f"\
         {datetime.now().strftime('%m/%d/%Y, %H:%M')} \
             Użytkownik: {user.username} przeniósł produkt z lokalizacji \
                 {delivery.location.name} do lokalizacji {to_location.name}\n"
     if to_location.work_zone == 4:
         delivery.complite_status = True
-        transact_names = {
-            "Utulizacja": "Utulizacja",
-            "Shiped": "Shiped",
-            "ANULACJA": "Anulacja"
-            }
+        
 
     if to_location.work_zone > delivery.location.work_zone:
-        create_transaction(
-            user=user, delivery=delivery, transaction_type=transact_names[to_location.name]
-        )
+        if to_location.name in transact_names:
+            create_transaction(
+                user=user, delivery=delivery, transaction_type= transact_names[to_location.name]
+            )
+        else:
+            create_transaction(
+                user=user, delivery=delivery, transaction_type= "Relocate"
+            )
     elif to_location.work_zone == delivery.location.work_zone:
          create_transaction(
             user=user, delivery=delivery, transaction_type="Optimization"
