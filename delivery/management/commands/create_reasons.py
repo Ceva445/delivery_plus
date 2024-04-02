892eddb388d1a862bfd5aabdd9d5e258203ca0af
MrYuriy <yuriybabiyk@gmail.com>
2024-04-02 17:27:50 +0200

    Add new creation logic reason code

--- delivery/management/commands/create_reasons.py
+++ delivery/management/commands/create_reasons.py
@@ -4,15 +4,18 @@ import xlrd
 
 
 class Command(BaseCommand):
-    help = "Auto create Reasone code"
+    help = "Auto create Reason code"
 
     def handle(self, *args, **options):
         workbook = xlrd.open_workbook("reasons.xls")
         sheet = workbook.sheet_by_index(0)
+        existing_reasons = set(ReasoneComment.objects.values_list("name", flat=True))
 
-        supp_inst = [
-            ReasoneComment(name=sheet.row_values(row)[0]) for row in range(sheet.nrows)
+        new_reasons = [
+            ReasoneComment(name=sheet.row_values(row)[0])
+            for row in range(sheet.nrows)
+            if sheet.row_values(row)[0] not in existing_reasons
         ]
-        ReasoneComment.objects.bulk_create(supp_inst)
+        ReasoneComment.objects.bulk_create(new_reasons)
 
-        self.stdout.write("Reasons create successful")
+        self.stdout.write("Reasons created successfully")
